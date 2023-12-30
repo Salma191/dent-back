200d83b72374569c7ed729882e773e97cea29adf
JALAOUI Salma <116913855+Salma191@users.noreply.github.com>
2023-12-30 16:51:54 +0100

    Update UserController.java

--- Dent/src/main/java/org/ENSAJ/controller/UserController.java
+++ Dent/src/main/java/org/ENSAJ/controller/UserController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://dent-frontend.vercel.app")
 public class UserController {
 
     private final UserService userService;
